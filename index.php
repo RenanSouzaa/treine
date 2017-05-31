a151480bba847af608c23805c9effc017076a70e
renan <renan@Renan>
2017-05-31 17:38:25 -0300

    Alterações e inclusoes ok!

--- index.php
+++ index.php
@@ -10,8 +10,12 @@ and open the template in the editor.
         <title>Treinamento - HTML 5 - Upinside</title>
     </head>
     <body>
+        <p>Hoje começou no desenvolvimento WEB  :)</p>
         <?php
         // put your code here
         ?>
+        <ul>
+            <h1>Bora la!</h1>
+        </ul>
     </body>
 </html>
